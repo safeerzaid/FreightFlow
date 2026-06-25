16e0658481b298255450e3c8b996da8282a51617
safeer <safeerzaid4@gmail.com>
2026-06-25 11:30:29 +0530

    feat: add RC, insurance, and rating fields to Driver model

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -22,6 +22,23 @@ const driverSchema = mongoose.Schema({
     unique: true,
     required: true
   },
+
+  RC:{
+    type:String,
+    unique:true,
+    required:true
+  },
+
+  insurance:{
+    type: String,
+    required: true,
+  },
+
+  rating: {
+    type: Number,
+    default:0
+  },
+
   isApproved: {
     type: Boolean,
     default: false
